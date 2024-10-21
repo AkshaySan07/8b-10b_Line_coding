ffabe4120899521a01a76522afea52eca0224725
AkshaySan07 <akshay922004@gmail.com>
2024-10-21 11:25:30 +0530

    Update encoder8b_10b.v

--- encoder8b_10b.v
+++ encoder8b_10b.v
@@ -43,6 +43,19 @@ module encoder_8b10b (
     end
 
     always @(*) begin
+
+        count = 0;  // Initialize count to 0
+        for (i = 0; i < 10; i = i + 1) begin
+            count = count + encoded[i];  // Increment count for every '1' found in the vector
+        end
+        
+        if(count > 4'd5)
+            new_rd = 1'b1;
+        else if(count < 4'd5)
+            new_rd = 1'b0;
+        else 
+            new_rd = rd;       // count == 5 then equal number of 0's and 1's
+
         case (rd)
         1'b0: begin
             case (data_in[7:5])
@@ -66,31 +79,31 @@ module encoder_8b10b (
                 5'd4 : encoded[9:4] = 6'b110101;
                 5'd5 : encoded[9:4] = 6'b101001;  // Does not depend on RD
                 5'd6 : encoded[9:4] = 6'b011001;
-                5'd7 : encoded[9:4] = 6'b000111;
-                5'd8 : encoded[9:4] = 6'b000110;
+                5'd7 : encoded[9:4] = 6'b111000;
+                5'd8 : encoded[9:4] = 6'b111001;
                 5'd9 : encoded[9:4] = 6'b100101;  // Does not depend on RD
-                5'd10: encoded[9:4] = 6'b101010;
-                5'd11: encoded[9:4] = 6'b001101;
-                5'd12: encoded[9:4] = 6'b110010;
-                5'd13: encoded[9:4] = 6'b101101;  // Does not depend on RD
-                5'd14: encoded[9:4] = 6'b011001;
-                5'd15: encoded[9:4] = 6'b101100;
-                5'd16: encoded[9:4] = 6'b100100;
-                5'd17: encoded[9:4] = 6'b011100;
-                5'd18: encoded[9:4] = 6'b101100;
-                5'd19: encoded[9:4] = 6'b001101;
-                5'd20: encoded[9:4] = 6'b110100;
+                5'd10: encoded[9:4] = 6'b010101;
+                5'd11: encoded[9:4] = 6'b110100;
+                5'd12: encoded[9:4] = 6'b001101;
+                5'd13: encoded[9:4] = 6'b101100;  // Does not depend on RD
+                5'd14: encoded[9:4] = 6'b011100;
+                5'd15: encoded[9:4] = 6'b010111;
+                5'd16: encoded[9:4] = 6'b011011;
+                5'd17: encoded[9:4] = 6'b100011;
+                5'd18: encoded[9:4] = 6'b010011;
+                5'd19: encoded[9:4] = 6'b110010;
+                5'd20: encoded[9:4] = 6'b001011;
                 5'd21: encoded[9:4] = 6'b101010;  // Does not depend on RD
-                5'd22: encoded[9:4] = 6'b011001;
-                5'd23: encoded[9:4] = 6'b000101;
-                5'd24: encoded[9:4] = 6'b001100;
+                5'd22: encoded[9:4] = 6'b011010;
+                5'd23: encoded[9:4] = 6'b111010;
+                5'd24: encoded[9:4] = 6'b110011;
                 5'd25: encoded[9:4] = 6'b100110;  // Does not depend on RD
-                5'd26: encoded[9:4] = 6'b101010;
-                5'd27: encoded[9:4] = 6'b110010;
-                5'd28: encoded[9:4] = 6'b000110;
-                5'd29: encoded[9:4] = 6'b011110;
-                5'd30: encoded[9:4] = 6'b110011;
-                5'd31: encoded[9:4] = 6'b111100;  // Does not depend on RD
+                5'd26: encoded[9:4] = 6'b010110;
+                5'd27: encoded[9:4] = 6'b110110;
+                5'd28: encoded[9:4] = 6'b001110;
+                5'd29: encoded[9:4] = 6'b101110;
+                5'd30: encoded[9:4] = 6'b011110;
+                5'd31: encoded[9:4] = 6'b101011;  // Does not depend on RD
                 default: encoded[9:4] = 6'bxxxxxx; // Handle unexpected cases
             endcase
         end
@@ -120,45 +133,35 @@ module encoder_8b10b (
                 5'd7 : encoded[9:4] = 6'b000111;
                 5'd8 : encoded[9:4] = 6'b000110;
                 5'd9 : encoded[9:4] = 6'b100101;  // Does not depend on RD
-                5'd10: encoded[9:4] = 6'b010001;
-                5'd11: encoded[9:4] = 6'b110010;
-                5'd12: encoded[9:4] = 6'b001001;
-                5'd13: encoded[9:4] = 6'b101101;  // Does not depend on RD
-                5'd14: encoded[9:4] = 6'b011001;
-                5'd15: encoded[9:4] = 6'b010011;
-                5'd16: encoded[9:4] = 6'b011011;
+                5'd10: encoded[9:4] = 6'b010101;
+                5'd11: encoded[9:4] = 6'b110100;
+                5'd12: encoded[9:4] = 6'b001101;
+                5'd13: encoded[9:4] = 6'b101100;  // Does not depend on RD
+                5'd14: encoded[9:4] = 6'b011100;
+                5'd15: encoded[9:4] = 6'b101000;
+                5'd16: encoded[9:4] = 6'b100100;
                 5'd17: encoded[9:4] = 6'b100011;
                 5'd18: encoded[9:4] = 6'b010011;
                 5'd19: encoded[9:4] = 6'b110010;
                 5'd20: encoded[9:4] = 6'b001011;
                 5'd21: encoded[9:4] = 6'b101010;  // Does not depend on RD
-                5'd22: encoded[9:4] = 6'b011001;
+                5'd22: encoded[9:4] = 6'b011010;
                 5'd23: encoded[9:4] = 6'b000101;
-                5'd24: encoded[9:4] = 6'b110011;
+                5'd24: encoded[9:4] = 6'b001100;
                 5'd25: encoded[9:4] = 6'b100110;  // Does not depend on RD
-                5'd26: encoded[9:4] = 6'b010101;
-                5'd27: encoded[9:4] = 6'b001101;
-                5'd28: encoded[9:4] = 6'b111000;
-                5'd29: encoded[9:4] = 6'b100001;
-                5'd30: encoded[9:4] = 6'b001100;
-                5'd31: encoded[9:4] = 6'b111100;  // Does not depend on RD
+                5'd26: encoded[9:4] = 6'b010110;
+                5'd27: encoded[9:4] = 6'b001001;
+                5'd28: encoded[9:4] = 6'b001110;
+                5'd29: encoded[9:4] = 6'b010001;
+                5'd30: encoded[9:4] = 6'b100001;
+                5'd31: encoded[9:4] = 6'b010100;  // Does not depend on RD
                 default: encoded[9:4] = 6'bxxxxxx; // Handle unexpected cases
             endcase
         end
         endcase
 
 
-        count = 0;  // Initialize count to 0
-        for (i = 0; i < 10; i = i + 1) begin
-            count = count + encoded[i];  // Increment count for every '1' found in the vector
-        end
         
-        if(count > 4'd5)
-            new_rd = 1'b1;
-        else if(count < 4'd5)
-            new_rd = 1'b0;
-        else 
-            new_rd = rd;
             
     end
 
